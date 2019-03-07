ebde17ce3561f222b89ece22eac3fbb7e43cc3bd
Iain <iain@polevaultweb.com>
2019-03-07 16:04:11 +0000

    Remove manual bootstrapping of spinup mu plugin.

--- app/App.php
+++ app/App.php
@@ -8,21 +8,6 @@ class App {
 	 * 	Load custom site code
 	 */
 	public function register() {
-		$this->load_spinupwp_mu_plugin();
-	}
-
-	/**
-	 * Bootstrap the SpinupWP mu-plugin.
-	 */
-	protected function load_spinupwp_mu_plugin() {
-		if ( ! defined( 'SPINUPWP_CACHE_PATH' ) ) {
-			return;
-		}
-
-		$vendor_dir = dirname( __DIR__ ) . '/vendor/deliciousbrains/spinupwp-mu-plugin';
-		if ( file_exists( $vendor_dir ) ) {
-			require_once $vendor_dir . '/src/spinupwp.php';
-		}
 	}
 
 	/**
