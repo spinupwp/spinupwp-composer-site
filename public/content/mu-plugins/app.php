190d301f879fdf265fc0d38f97e3001ea8526b39
Iain <iain@polevaultweb.com>
2019-04-11 13:01:58 +0100

    Ensure the Spinupwp plugin is bootstrapped.

--- public/content/mu-plugins/app.php
+++ public/content/mu-plugins/app.php
@@ -1,3 +1,8 @@
 <?php
 // Bootstrap our site code
 ( new DeliciousBrains\SpinupWPComposerSite\App() )->register();
+
+$spinupwp_file =  __DIR__ . '/spinupwp-mu-plugin/src/spinupwp.php';
+if ( file_exists( $spinupwp_file ) && defined( 'SPINUPWP_CACHE_PATH' ) && ! empty( SPINUPWP_CACHE_PATH ) ) {
+	require_once $spinupwp_file;
+}
