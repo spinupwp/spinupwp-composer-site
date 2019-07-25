17ab11401eacf6b22d066190e8be060c32ec9e3d
Iain <iain@polevaultweb.com>
2019-07-25 10:58:06 +0100

    Swap SpinupWP mu-plugin with new plugin,

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,2 +1,1 @@
 <?php
-require_once dirname( __DIR__ ) . '/spinupwp.php';
--- config/spinupwp.php
+++
@@ -1,6 +0,0 @@
-<?php
-$domain = parse_url( WP_HOME, PHP_URL_HOST );
-
-define( 'WP_CACHE_KEY_SALT', $domain );
-define( 'WP_REDIS_SELECTIVE_FLUSH', true );
-define( 'SPINUPWP_CACHE_PATH', '/cache/' . $domain );
