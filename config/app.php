38b7662596c183603eb495474a8894ab7d598910
Ross Wintle <rosswintle@users.noreply.github.com>
2021-11-12 18:14:23 +0000

    Fix to coding standards as best we can
    Will have to wait for other #14 and #15 to be merged to do it properly

--- config/app.php
+++ config/app.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Your base production configuration goes in this file. Environment-specific
  * overrides go in their respective config/environments/{{WP_ENV}}.php file.
@@ -9,7 +8,7 @@
  * can.
  */
 
-use \DeliciousBrains\SpinupWPComposerSite\App;
+use DeliciousBrains\SpinupWPComposerSite\App;
 
 $root_dir    = dirname( __DIR__ );
 $webroot_dir = $root_dir . '/public';
@@ -23,7 +22,7 @@ Env::init();
  * Use Dotenv to set required environment variables and load .env file in root
  */
 $env_files = file_exists( $root_dir . '/.env.local' ) ? [ '.env', '.env.local' ] : [ '.env' ];
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir, $env_files, false);
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable( $root_dir, $env_files, false );
 if ( file_exists( $root_dir . '/.env' ) ) {
 	$dotenv->load();
 	$dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
@@ -40,36 +39,36 @@ App::define( 'CONTENT_DIR', '/content' );
 App::define( 'WP_CONTENT_DIR', $webroot_dir . CONTENT_DIR );
 App::define( 'WP_CONTENT_URL', WP_HOME . CONTENT_DIR );
 
-App::define('DB_NAME', env('DB_NAME'));
-App::define('DB_USER', env('DB_USER'));
-App::define('DB_PASSWORD', env('DB_PASSWORD'));
-App::define('DB_HOST', env('DB_HOST') ?: 'localhost');
-App::define('DB_CHARSET', 'utf8mb4');
-App::define('DB_COLLATE', '');
-$table_prefix = env('DB_PREFIX') ?: 'wp_';
+App::define( 'DB_NAME', env( 'DB_NAME' ) );
+App::define( 'DB_USER', env( 'DB_USER' ) );
+App::define( 'DB_PASSWORD', env( 'DB_PASSWORD' ) );
+App::define( 'DB_HOST', env( 'DB_HOST' ) ?: 'localhost' );
+App::define( 'DB_CHARSET', 'utf8mb4' );
+App::define( 'DB_COLLATE', '' );
+$table_prefix = env( 'DB_PREFIX' ) ?: 'wp_';
 
 // Set other constants from env file.
-foreach( array_keys($dotenv->load()) as $key ) {
-	App::define($key, env( $key ));
+foreach ( array_keys( $dotenv->load() ) as $key ) {
+	App::define( $key, env( $key ) );
 }
 
-// Environment config
+// Environment config.
 $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
 if ( file_exists( $env_config ) ) {
 	require_once $env_config;
 }
 
-App::define('AUTOMATIC_UPDATER_DISABLED', true);
-App::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
-// Disable the plugin and theme file editor in the admin
-App::define('DISALLOW_FILE_EDIT', true);
-// Disable plugin and theme updates and installation from the admin
-App::define('DISALLOW_FILE_MODS', true);
+App::define( 'AUTOMATIC_UPDATER_DISABLED', true );
+App::define( 'DISABLE_WP_CRON', env( 'DISABLE_WP_CRON' ) ?: false );
+// Disable the plugin and theme file editor in the admin.
+App::define( 'DISALLOW_FILE_EDIT', true );
+// Disable plugin and theme updates and installation from the admin.
+App::define( 'DISALLOW_FILE_MODS', true );
 
-// Secret keys
+// Secret keys.
 if ( ! file_exists( __DIR__ . '/keys.php' ) ) {
 	$keys = file_get_contents( 'https://api.wordpress.org/secret-key/1.1/salt/' );
-	file_put_contents( __DIR__ . '/keys.php', '<?php use ' . App::class . '; ' . str_replace( 'define(', 'App::define(', $keys ) );
+	file_put_contents( __DIR__ . '/keys.php', '<?php use ' . App::class . ";\n" . str_replace( 'define(', 'App::define(', $keys ) );
 }
 include __DIR__ . '/keys.php';
 
