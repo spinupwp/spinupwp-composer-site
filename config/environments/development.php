0665fef901cce38833bc676df043ed0b12ae9c80
Ross Wintle <rosswintle@users.noreply.github.com>
2021-11-12 17:02:24 +0000

    Improve environment-specific config and add documentation

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,10 +1,20 @@
 <?php
+/**
+ * This file contains a base config for a development environment.
+ *
+ * It can be overridden using a .env or .env.local file in the root directory
+ * of the project.
+ *
+ * Always use App::define() to define new constants in this file.
+ */
 
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', true);
+use \DeliciousBrains\SpinupWPComposerSite\App;
+
+App::define('WP_DEBUG', true);
+App::define('WP_DEBUG_LOG', true);
+App::define('WP_DEBUG_DISPLAY', false);
+App::define('SCRIPT_DEBUG', true);
 
 @ini_set( 'display_errors', E_ALL );
 
-define( 'DISALLOW_FILE_MODS', false );
+App::define( 'DISALLOW_FILE_MODS', false );
--- config/environments/production.php
+++ config/environments/production.php
@@ -1,2 +1,11 @@
 <?php
-// Add any production specific config
+/**
+ * This file contains a base config for a production environment.
+ *
+ * It can be overridden using a .env or .env.local file in the root directory
+ * of the project
+ *
+ * Always use App::define() to define new constants in this file.
+ */
+
+// App::define( CONSTANT, 'production_value' );
